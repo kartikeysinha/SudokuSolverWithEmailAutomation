d53c81c2cb9b1106633530e1d33c3637f21c5399
siddharth-vagavolu <svagavol@edu.uwaterloo.ca>
2020-01-14 20:59:12 -0500

    // new functions //

--- main.py
+++ main.py
@@ -2,3 +2,32 @@ print("Hello World")
 print("Trying Commit")
 print("Trying to Commit")
 
+
+def Convert(string):
+    li = list(string.split(" "))
+    return li
+
+
+# Driver code
+str1 = input()
+str2 = input()
+str3 = input()
+str4 = input()
+str5 = input()
+str6 = input()
+str7 = input()
+str8 = input()
+str9 = input()
+
+sudoku = ([str1,
+       str2,
+       str3,
+       str4,
+       str5,
+       str6,
+       str7,
+       str8,
+       str9])
+
+
+
